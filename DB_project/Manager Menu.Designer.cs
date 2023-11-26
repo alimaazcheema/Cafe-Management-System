a2059cfb38f81b14cadd18a8343b55b37ec44b3c
Muhammad Bin Basit <Muhammad Bin Basit@LAPTOP-JL2TMLJA>
2023-11-26 17:02:12 +0500

    manager menu

---
+++ DB_project/Manager Menu.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace DB_project
+{
+    partial class Manager_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Create_Report = new System.Windows.Forms.Button();
+            this.Delete_menu = new System.Windows.Forms.Button();
+            this.Update_menu = new System.Windows.Forms.Button();
+            this.Add_menu = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(113)))), ((int)(((byte)(97)))));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 109);
+            this.panel1.TabIndex = 35;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(234)))), ((int)(((byte)(216)))));
+            this.label6.Location = new System.Drawing.Point(280, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(236, 37);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Manager Menu";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Create_Report
+            // 
+            this.Create_Report.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.Create_Report.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Create_Report.Location = new System.Drawing.Point(409, 265);
+            this.Create_Report.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Create_Report.Name = "Create_Report";
+            this.Create_Report.Size = new System.Drawing.Size(215, 57);
+            this.Create_Report.TabIndex = 42;
+            this.Create_Report.Text = "Create Report and Analysis";
+            this.Create_Report.UseVisualStyleBackColor = false;
+            this.Create_Report.Click += new System.EventHandler(this.create_report);
+            // 
+            // Delete_menu
+            // 
+            this.Delete_menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.Delete_menu.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Delete_menu.Location = new System.Drawing.Point(176, 265);
+            this.Delete_menu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Delete_menu.Name = "Delete_menu";
+            this.Delete_menu.Size = new System.Drawing.Size(215, 57);
+            this.Delete_menu.TabIndex = 41;
+            this.Delete_menu.Text = "Delete Menu Item";
+            this.Delete_menu.UseVisualStyleBackColor = false;
+            this.Delete_menu.Click += new System.EventHandler(this.delete_menu);
+            // 
+            // Update_menu
+            // 
+            this.Update_menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.Update_menu.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Update_menu.Location = new System.Drawing.Point(409, 182);
+            this.Update_menu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Update_menu.Name = "Update_menu";
+            this.Update_menu.Size = new System.Drawing.Size(215, 57);
+            this.Update_menu.TabIndex = 40;
+            this.Update_menu.Text = "Update Menu Item";
+            this.Update_menu.UseVisualStyleBackColor = false;
+            this.Update_menu.Click += new System.EventHandler(this.update_menu);
+            // 
+            // Add_menu
+            // 
+            this.Add_menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.Add_menu.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Add_menu.Location = new System.Drawing.Point(176, 182);
+            this.Add_menu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Add_menu.Name = "Add_menu";
+            this.Add_menu.Size = new System.Drawing.Size(215, 57);
+            this.Add_menu.TabIndex = 39;
+            this.Add_menu.Text = "Add Menu Item";
+            this.Add_menu.UseVisualStyleBackColor = false;
+            this.Add_menu.Click += new System.EventHandler(this.add_menu);
+            // 
+            // Manager_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(139)))), ((int)(((byte)(116)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Create_Report);
+            this.Controls.Add(this.Delete_menu);
+            this.Controls.Add(this.Update_menu);
+            this.Controls.Add(this.Add_menu);
+            this.Controls.Add(this.panel1);
+            this.Name = "Manager_Menu";
+            this.Text = "Manager_Menu";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label6;
+        private Button Create_Report;
+        private Button Delete_menu;
+        private Button Update_menu;
+        private Button Add_menu;
+    }
+}
---
+++ DB_project/Manager Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_project
+{
+    public partial class Manager_Menu : Form
+    {
+        public Manager_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_2(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void add_menu(object sender, EventArgs e)
+        {
+
+        }
+
+        private void update_menu(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_menu(object sender, EventArgs e)
+        {
+
+        }
+
+        private void create_report(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
